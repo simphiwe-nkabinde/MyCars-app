816b62446744e4aad51c7280f3c8d0407f14be4c
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2021-06-27 18:36:36 +0200

    client-side routing issue

--- cars_server.js
+++ cars_server.js
@@ -128,7 +128,7 @@ app.delete('/car/:username', (req, resp) => {
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'frontend/build')));
-    app.get('*',(req,res)=> {res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    app.get('/*',(req,res)=> {res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 }
 
